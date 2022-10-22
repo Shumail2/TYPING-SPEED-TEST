7407174810679450bede6124571672158bf3c763
Shumail <info.clothingmagnet@gmail.com>
2022-10-22 12:25:23 +0530

    Initial commit

---
+++ type.py
@@ -0,0 +1,37 @@
+from time import *
+import random as r
+
+
+
+def mistake(partest,usertest):
+    error = 0
+    for i in range(len(partest)):
+        try:
+            if partest[i] != usertest[i]:
+                error = error + 1
+        except:
+            error = error + 1
+
+    return error
+
+def speed_time(time_s,time_e,userinput):
+    time_delay = time_e - time_s
+    time_R = round(time_delay,2)
+    speed = len(userinput)/ time_R
+    return round(speed)
+
+
+test =[" If you have gained enough knowledge about the Python programming language and want to get selected in any company at a good salary, this video is for you."
+"Python project for beginners in Hindi, you will learn critical skills that will make you sharp in Python. Python is one of the most used languages currently, and the demand for programmers in the market is not slowing down."]
+test1 = r.choice(test)
+print("      ***** typing speed *****")
+print(test1)
+print()
+print()
+
+time_1 = time()
+testinput=input(" Enter : ")
+time_2 = time() 
+
+print('Speed : ',speed_time(time_1,time_2,testinput)," w/sec")
+print("Error : ",mistake(test1,testinput))
